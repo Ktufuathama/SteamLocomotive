6ca8c1e9b611b00b5b23fc124aac4100178e294a
Ktufuathama <31010254+Ktufuathama@users.noreply.github.com>
2021-10-20 18:11:43 -0600

    Update SteamLocomotive with class based BufferCellArrays. Also added Import and Export.

--- SteamLocomotive.psm1
+++ SteamLocomotive.psm1
@@ -1,35 +1,249 @@
 
 <#
-                                                                                     
-                                                                                     
-                                                                                     
-                                                                                     
-                                                                                     
-      ====        ________                 ___________                               
-  _D _|  |_______/        \__I_I______===__|_________|                               
-   |(_)---  |   H\________/ |   |         =|___ ___|      _________________          
-   /     |  |   H  |  |     |   |          ||_| |_||     _|                \______A  
-  |      |  |   H  |__---------------------| [___] |   =|                        |   
-  | ________|___H__/__|_____/[][]~\\_______|       |   -|                        |   
-  |/ |   |-----------I_____I [][] []  D    |=======|____|________________________|_  
-__/ =| o |=-~~\  /~~\  /~~\  /~~\ ____Y____________|__|__________________________|_  
- |/-=|___|=    ||    ||    ||    |_____/~\\___/         |_D__D__D_|  |_D__D__D_|     
-   \_/     \O=====O=====O=====O_/      \_/               \_/   \_/    \_/   \_/      
+ == SteamLocomotive ==
+                       (  ) (@@) ( ) (@) ()   @@  O   @@    O         @@      o        
+                   (@@@)                                                               
+              (    )                                                                   
+           (@@@@)                                                                      
+         (   )                                                                         
+        ====        ________                 ___________                               
+    _D _|  |_______/        \__I_I______===__|_________|                               
+     |(_)---  |   H\________/ |   |         =|___ ___|      _________________          
+     /     |  |   H  |  |     |   |          ||_| |_||     _|                \______A  
+    |      |  |   H  |__---------------------| [___] |   =|                        |   
+    | ________|___H__/__|_____/[][]~\\_______|       |   -|                        |   
+    |/ |   |-----------I_____I [][] []  D    |=======|____|________________________|_  
+  __/ =| o |=-O=====O=====O=====O~\ ____Y____________|__|__________________________|_  
+   |/-=|___|=    ||    ||    ||    |_____/~\\___/         |_D__D__D_|  |_D__D__D_|     
+     \_/     \__/  \__/  \__/  \__/      \_/               \_/   \_/    \_/   \_/      
 #>
-function Start-SteamLocomotive {
-  <#
-    $Coord = @{x=0;y=0}
-    while ($true) {
-      switch ($host.UI.RawUI.readKey().Character) {
-        'd' { if ($Coord.X -le ($host.UI.RawUI.WindowSize.Width - 1)) {$Coord.X++} }
-        'a' { if ($Coord.X -gt 0) {$Coord.X--} }
-        'w' { if ($Coord.Y -gt 0) {$Coord.Y--} }
-        's' { if ($Coord.Y -le $host.UI.RawUI.WindowSize.Height) {$Coord.Y++} }
+
+class SerializedBufferCellArray
+{
+  [int]$Index = 0
+  [system.management.automation.host.buffercell[]]$Cells
+
+  SerializedBufferCellArray([int]$index)
+  {
+    $this.Index = $index
+  }
+
+  SerializedBufferCellArray([int]$index, [system.management.automation.host.buffercell[]]$bufferCellArray)
+  {
+    $this.Index = $index
+    $this.Cells = $bufferCellArray
+  }
+
+  [string] ToString()
+  {
+    return [string]($this.Cells.Character -join '')
+  }
+
+  static [serializedbuffercellarray[]] Serialize([system.management.automation.host.buffercell[,]]$inputObject)
+  {
+    for ($y = 0; $y -le $inputObject.getUpperBound(0); $y++) {
+      $SBCA = [serializedbuffercellarray]::new($y)
+      for ($x = 0; $x -le $inputObject.getUpperBound(1); $x++) {
+        $SBCA.Cells += [system.management.automation.host.buffercell]($inputObject[$y, $x])
       }
-      #BoundsLogic
-      $host.UI.RawUI.setBufferContents($Coord, $B1)
+      [serializedbuffercellarray[]]$SBCAs += $SBCA
     }
-  #>
+    return $SBCAs
+  }
+
+  #Bug:Does not return multidimensional array?
+  static [system.management.automation.host.buffercell[,]] Deserialize([serializedbuffercellarray[]]$inputObject)
+  {
+    $a = @{X=($inputObject[0].Cells.Count - 1);Y=($inputObject.Count - 1)}
+    Write-Verbose "y:$($a.Y); x:$($a.X)"
+    $BCA = [system.management.automation.host.buffercell[,]]::new(($a.Y + 1), ($a.X + 1))
+    for ($y = 0; $y -le $a.Y; $y++) {
+      Write-Verbose "y:$($y)"
+      for ($x = 0; $x -le $a.X; $x++) {
+        if ($y -eq $a.Y) {
+          $BCA[$y, $x] = [system.management.automation.host.buffercell]$inputObject[$y].Cells[$x]
+        }
+        else {
+          $BCA[$y, $x] = [system.management.automation.host.buffercell]$inputObject[$y].Cells[$x]
+        }
+      }
+    }
+    return $BCA
+  }
+}
+
+function ConvertTo-BufferCellArray {
+  [cmdletbinding()]
+  param(
+    [string]$InputObject
+  )
+  $a = @{
+    Y = $InputObject.split("`n").Count
+    X = $InputObject.split("`n")[0].Length
+  }
+  $InputObject = $InputObject.replace("`n", "").replace("`r", "")
+  $BCA = [system.management.automation.host.buffercell[,]]::new($a.Y, $a.X)
+  $i = 0
+  Write-Verbose "X=$($a.X);Y=$($a.Y)"
+  for ($y = 0; $y -lt $a.Y; $y++) {
+    for ($x = 0; $x -lt $a.X; $x++) {
+      Write-Verbose "x=$($x);y=$($y);i=$($i)"
+      $BCA[$y, $x] = [system.management.automation.host.buffercell]::new(
+        $InputObject.toCharArray()[$i],
+        $host.UI.RawUI.ForegroundColor,
+        $host.UI.RawUI.BackgroundColor,
+        [system.management.automation.host.buffercelltype]::Complete)
+      $i++
+    }
+  }
+  return ,$BCA
+}
+
+function ConvertTo-SerializedBufferCellArray {
+  [cmdletbinding()]
+  param(
+      [parameter(valuefrompipeline)]
+    [system.management.automation.host.buffercell[,]]$BufferCellArray
+  )
+  process {
+    $BCA += $BufferCellArray
+  }
+  end {
+    Write-Verbose "y:$($BCA.getUpperBound(0)); x:$($BCA.getUpperBound(1))"
+    for ($y = 0; $y -le $BCA.getUpperBound(0); $y++) {
+      Write-Verbose "y:$($y)"
+      $SBCA = [serializedbuffercellarray]::new($y)
+      for ($x = 0; $x -le $BCA.getUpperBound(1); $x++) {
+        $SBCA.Cells += [system.management.automation.host.buffercell]($BCA[$y, $x])
+      }
+      Write-Debug "$($SBCA.toString())"
+      [serializedbuffercellarray[]]$SBCAs += $SBCA
+    }
+    return $SBCAs
+  }
+}
+
+function ConvertFrom-SerializedBufferCellArray {
+  [cmdletbinding()]
+  param(
+    #[serializedbuffercellarray[]]
+      [parameter(valuefrompipeline)]
+    [object[]]$SerializedBufferCellArray
+  )
+  process {
+    $SBCA += $SerializedBufferCellArray
+  }
+  end {
+    $a = @{X=($SBCA[0].Cells.Count - 1);Y=($SBCA.Count - 1)}
+    Write-Verbose "y:$($a.Y); x:$($a.X)"
+    $BCA = [system.management.automation.host.buffercell[,]]::new(($a.Y + 1), ($a.X + 1))
+    for ($y = 0; $y -le $a.Y; $y++) {
+      Write-Verbose "y:$($y)"
+      for ($x = 0; $x -le $a.X; $x++) {
+        if ($y -eq $a.Y) {
+          $BCA[$y, $x] = [system.management.automation.host.buffercell]$SBCA[$y].Cells[$x]
+        }
+        else {
+          $BCA[$y, $x] = [system.management.automation.host.buffercell]$SBCA[$y].Cells[$x]
+        }
+      }
+    }
+    return ,$BCA
+  }
+}
+
+function Compress-BufferCellArray {
+  [cmdletbinding()]
+  param(
+      [parameter(valuefrompipeline)]
+    [string[]]$InputObject
+  )
+  process {
+    $Object += $InputObject
+  }
+  end {
+    $Object = [regex]::replace($Object, '"Character":"',        '~1')
+    $Object = [regex]::replace($Object, '","ForegroundColor":', '~2')
+    $Object = [regex]::replace($Object, ',"BackgroundColor":',  '~3')
+    $Object = [regex]::replace($Object, ',"BufferCellType":',   '~4')
+    $Object = [regex]::replace($Object, '"Index"',              '~5')
+    $Object = [regex]::replace($Object, '"Cells"',              '~6')
+    return $Object
+  }
+}
+
+function Expand-BufferCellArray {
+  [cmdletbinding()]
+  param(
+      [parameter(valuefrompipeline)]
+    [string[]]$InputObject
+  )
+  process {
+    $Object += $InputObject
+  }
+  end {
+    $Object = [regex]::replace($Object, '~1', '"Character":"')
+    $Object = [regex]::replace($Object, '~2', '","ForegroundColor":')
+    $Object = [regex]::replace($Object, '~3', ',"BackgroundColor":')
+    $Object = [regex]::replace($Object, '~4', ',"BufferCellType":')
+    $Object = [regex]::replace($Object, '~5', '"Index"')
+    $Object = [regex]::replace($Object, '~6', '"Cells"')
+    return $Object
+  }
+}
+
+function Export-BufferCellArray {
+  [cmdletbinding()]
+  param(
+      [parameter(valuefrompipeline)]
+    [system.management.automation.host.buffercell[,]]$BufferCellArray,
+    [string]$Path
+  )
+  process {
+    $BCA += $BufferCellArray
+  }
+  end {
+    [serializedbuffercellarray[]]$SBCA = ConvertTo-SerializedBufferCellArray -bufferCellArray $BCA
+    $SBCA `
+      | ConvertTo-Json -depth 10 -compress `
+      | Compress-BufferCellArray `
+      | Set-Content -path $Path
+  }
+}
+
+function Import-BufferCellArray {
+  [cmdletbinding()]
+  param(
+    [string]$Path
+  )
+  $BCA = Get-Content -path $Path -raw -errorAction 'Stop' `
+    | Expand-BufferCellArray `
+    | ConvertFrom-Json `
+    | ConvertFrom-SerializedBufferCellArray
+  return ,$BCA
+}
+
+function Invoke-Serializer {
+  [cmdletbinding()]
+  param(
+    [parameter(valuefrompipeline)]
+    [object]$InputObject,
+    [switch]$Deserialize
+  )
+  process {
+    if ($Deserialize) {
+      $Return = [system.management.automation.psserializer]::deserialize($InputObject)
+    }
+    else {
+      $Return = [system.management.automation.psserializer]::serialize($InputObject)
+    }
+  }
+  end {
+    return $Return
+  }
+}
+
+function Start-SteamLocomotive {
   $T1 = 
 @"
                      (@@@) (   ) (@@) (  ) @@   ()   @@   O    @@      O       @       
@@ -138,23 +352,23 @@ function Start-SteamLocomotive {
    |/-=|___|=    ||    ||    ||    |_____/~\\___/         |_D__D__D_|  |_D__D__D_|     
      \_/     \O=====O=====O=====O_/      \_/               \_/   \_/    \_/   \_/      
 "@
-  $TD = 
+  $TE = 
 @"
-                                        @                                              
-          * @       *     ^        *   *                                               
-             *@       *        *@ *                                                    
-               # @*   *    * *#@*                                                      
-            *@  # *@ **# @#*    @*  *                                                  
-        ====   *   *_*#_*_*_* @ @     *      ___________                               
-    _D _|  |____*__/  *     \*_*_I_*_*__===__|_________|                               
-     |(_)---  |    H\#__#___* |   |         =|___ ___|      _________________          
-     /     |  |  @   |  |     |   |          ||_| |_||     _|                \______A  
-    |      |  |   H  |__---------------------| [___] |   =|                        |   
-    | ________|___H__/__|_____/[][]~\\_______|       |   -|                        |   
-    |/ |   |-----------I_____I [][] []  D    |=======|____|________________________|_  
-  __/ =| o |=-~~\  /~~\  /~~\  /~~\ ____Y____________|__|__________________________|_  
-   |/-=|___|=    ||    ||    ||    |_____/~\\___/         |_D__D__D_|  |_D__D__D_|     
-     \_/     \O=====O=====O=====O_/      \_/               \_/   \_/    \_/   \_/      
+                                                                                       
+                                                                                       
+                                                                                       
+                                                                                       
+                                                                                       
+                                                                                       
+                                                                                       
+                                                                                       
+                                                                                       
+                                                                                       
+                                                                                       
+                                                                                       
+                                                                                       
+                                                                                       
+                                                                                       
 "@
   $B1 = ConvertTo-BufferCellArray $T1
   $B2 = ConvertTo-BufferCellArray $T2
@@ -162,14 +376,15 @@ function Start-SteamLocomotive {
   $B4 = ConvertTo-BufferCellArray $T4
   $B5 = ConvertTo-BufferCellArray $T5
   $B6 = ConvertTo-BufferCellArray $T6
-  $BD = ConvertTo-BufferCellArray $TD
-  Clear-Host
-  $c = @{x=($host.UI.RawUI.WindowSize.Width - 1);y=2}
+  $BE = ConvertTo-BufferCellArray $TE
+  $y = $host.UI.RawUI.CursorPosition.Y + 1
+  $x = $host.UI.RawUI.WindowSize.Width - 1
+  $c = @{x = $x; y = $y}
   $a = $true
   while ($true) {
     $c.X = ($c.X - 2)
     if ($c.X -eq 0 -or $c.X -eq 1) {
-      $host.UI.RawUI.setBufferContents($c, $BD)
+      $host.UI.RawUI.setBufferContents($c, $BE)
       break
     }
     switch (($c.X % 3)) {
@@ -200,121 +415,18 @@ function Start-SteamLocomotive {
         }
       }
     }
-    Start-Sleep -m 5
+    Start-Sleep -m 1
   }
-  Clear-Host
 }
 
-function Export-BufferCellArray {
-  [cmdletbinding()]
-  param(
-    [system.management.automation.host.buffercell[,]]$BCA,
-    [string]$Path
-  )
-  $Bounds = @{
-    X = $BCA.getUpperBound(1)
-    Y = $BCA.getUpperBound(0)
-  }
-  $Data = $Bounds, $BCA | ConvertTo-Json
-  Set-Content -path $Path -value $Data
-}
-
-function Import-BufferCellArray {
-  [cmdletbinding()]
-  param(
-    [string]$Path
-  )
-  $Data = Get-Content -path $Path -raw | ConvertFrom-Json
-  $BCA = [system.management.automation.host.buffercell[,]]::new($Data[0].Y + 1, $Data[0].X + 1)
-  $i = 0
-  Write-Verbose "X=$($BCA.getUpperBound(0));Y=$($BCA.getUpperBound(1))"
-  for ($y = 0; $y -lt $Data[0].Y + 1; $y++) {
-    for ($x = 0; $x -lt $Data[0].X + 1; $x++) {
-      Write-Verbose "x=$($x);y=$($y);i=$($i)"
-      $D = $Data[1][$i]
-      $BCA[$y, $x] = [system.management.automation.host.buffercell]::new(
-        $D.Character,
-        $D.ForegroundColor,
-        $D.BackgroundColor,
-        $D.BufferCellType)
-      $i++
+<#
+  $Coord = @{x=0;y=0}
+  while ($true) {
+    switch ($host.UI.RawUI.readKey().Character) {
+      'd' { if ($Coord.X -le ($host.UI.RawUI.WindowSize.Width - 1)) {$Coord.X++} }
+      'a' { if ($Coord.X -gt 0) {$Coord.X--} }
+      'w' { if ($Coord.Y -gt 0) {$Coord.Y--} }
+      's' { if ($Coord.Y -le $host.UI.RawUI.WindowSize.Height) {$Coord.Y++} }
     }
   }
-  return ,$BCA
-}
-
-function ConvertTo-BufferCellArray {
-  [cmdletbinding()]
-  param(
-    [string]$InputObject
-  )
-  $Bounds = @{
-    Y = $InputObject.split("`n").Count
-    X = $InputObject.split("`n")[0].Length
-  }
-  $InputObject = $InputObject.replace("`n", "").replace("`r", "")
-  $BCA = [system.management.automation.host.buffercell[,]]::new($Bounds.Y, $Bounds.X)
-  $i = 0
-  Write-Verbose "X=$($Bounds.X);Y=$($Bounds.Y)"
-  for ($y = 0; $y -lt $Bounds.Y; $y++) {
-    for ($x = 0; $x -lt $Bounds.X; $x++) {
-      Write-Verbose "x=$($x);y=$($y);i=$($i)"
-      $BCA[$y, $x] = [system.management.automation.host.buffercell]::new(
-        $InputObject.toCharArray()[$i],
-        $host.UI.RawUI.ForegroundColor,
-        $host.UI.RawUI.BackgroundColor,
-        [system.management.automation.host.buffercelltype]::Complete)
-      $i++
-    }
-  }
-  return ,$BCA
-}
-
-#WIP: Second merged object appears almost trimmed before and after.
-function Merge-BufferCellArray {
-  [cmdletbinding()]
-  param(
-    [system.management.automation.host.buffercell[,]]$BCA1,
-    [system.management.automation.host.buffercell[,]]$BCA2
-  )
-  $Bounds = @{X=0;Y=0}
-  if ($BCA1.getUpperBound(1) -gt $BCA2.getUpperBound(1)) {
-    $Bounds.X = $BCA1.getUpperBound(1)
-  }
-  else {
-    $Bounds.X = $BCA2.getUpperBound(1)
-  }
-  $Bounds.Y = $BCA1.getUpperBound(0) + $BCA2.getUpperBound(0)
-  $BCA = [system.management.automation.host.buffercell[,]]::new($Bounds.Y, $Bounds.X)
-  $i = 0
-  $o = $true
-  for ($y = 0; $y -lt $Bounds.Y; $y++) {
-    if ($y -le $BCA1.getUpperBound(0)) {
-      for ($x = 0; $x -lt $BCA1.getUpperBound(1); $x++) {
-        Write-Verbose "OBJ1: x=$($x);y=$($y);i=$($i)"
-        $BCA[$y, $x] = $BCA1[$y, $x]
-        $i++
-      }
-    }
-    else {
-      if ($o = $true) {
-        $i = 0
-        $o = $false
-      }
-      for ($x = 0; $x -lt $BCA2.getUpperBound(1); $x++) {
-        Write-Verbose "OBJ2: x=$($x);y=$($y);i=$($i)"
-        $BCA[$y, $x] = $BCA2[($y - $BCA1.getUpperBound(0)), $x]
-        $i++
-      }
-    }
-  }
-  <#
-    if ($BCA1.getUpperBound(0) -gt $BCA2.getUpperBound(0)) {
-      $Bounds.Y = $BCA1.getUpperBound(0)
-    }
-    else {
-      $Bounds.Y = $BCA2.getUpperBound(0)
-    }
-  #>
-  return ,$BCA
-}
+#>
